c3946ea8250f21aed6d1112fe185a6e7ae747344
dexSamir <samirah-bp215@code.edu.az>
2025-06-17 10:48:49 +0400

    update home page

--- src/home/Components/Header.jsx
+++ src/home/Components/Header.jsx
@@ -1,67 +1,73 @@
-import React, { useState } from 'react';
-import './header.css';
-import Logo from '../assets/logo.svg';
-import BurgerMenu from '../assets/burgerMenu.svg';
-import User from '../assets/profile-circle.svg';
-import SearchIcon from '../assets/search.svg';
-import ArrowDown from '../assets/arrow-down.svg';
-import BurgerMenuOverlay from './BurgerMenu';
+import React, { useState } from "react";
+import "./header.css";
+import Logo from "../assets/logo.svg";
+import BurgerMenu from "../assets/burgerMenu.svg";
+import User from "../assets/profile-circle.svg";
+import SearchIcon from "../assets/search.svg";
+import ArrowDown from "../assets/arrow-down.svg";
+import BurgerMenuOverlay from "./BurgerMenu";
+import { Link } from "react-router-dom";
 
 function Header() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [selectedCity, setSelectedCity] = useState('');
-  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => { 
+  const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-      <header className="header-container">
-        <div className="logo">
-          <img src={Logo} />
-        </div>
-        <button className="menu-button" onClick={toggleMenu}> 
+    <header className="header-container">
+      <div className="logo">
+        <img src={Logo} />
+      </div>
+      <button className="menu-button" onClick={toggleMenu}>
         <img src={BurgerMenu} alt="Menu" />
       </button>
-        <div className="search-area">
-          <div className="search-input-wrapper">
-            <img src={SearchIcon} alt="ss" />
-            <input
-              type="text"
-              placeholder="Axtar"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            {searchTerm && (
-              <button className="clear-button" onClick={() => setSearchTerm('')}>
-                <p>.</p>
-              </button>
-            )}
-          </div>
-          <div className="city-selector-wrapper">
-            <select
-              value={selectedCity}
-              onChange={(e) => setSelectedCity(e.target.value)}
-            >
-              <option value="" disabled>Şəhər seçin</option>
-              <option value="baku">Bakı</option>
-              <option value="ganja">Gəncə</option>
-              <option value="sumqayit">Sumqayıt</option>
-            </select>
-           <img src={ArrowDown} alt="arrow-down" />
-          </div>
+      <div className="search-area">
+        <div className="search-input-wrapper">
+          <img src={SearchIcon} alt="ss" />
+          <input
+            type="text"
+            placeholder="Axtar"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {searchTerm && (
+            <button className="clear-button" onClick={() => setSearchTerm("")}>
+              <p>.</p>
+            </button>
+          )}
         </div>
-        <button className="search-button-primary">Axtar</button>
+        <div className="city-selector-wrapper">
+          <select
+            value={selectedCity}
+            onChange={(e) => setSelectedCity(e.target.value)}
+          >
+            <option value="" disabled>
+              Şəhər seçin
+            </option>
+            <option value="baku">Bakı</option>
+            <option value="ganja">Gəncə</option>
+            <option value="sumqayit">Sumqayıt</option>
+          </select>
+          <img src={ArrowDown} alt="arrow-down" />
+        </div>
+      </div>
+      <button className="search-button-primary">Axtar</button>
+      <Link to="/login">
+        <button className="register-button">Daxil Ol</button>
+      </Link>
+
+      <Link to="/register">
         <button className="register-button">
-          Daxil Ol
-        </button>
-        <button className="register-button">
-          <img src={User} />
+          <img src={User} alt="User icon" />
           Qeydiyyat
         </button>
-        <BurgerMenuOverlay isOpen={isMenuOpen} onClose={toggleMenu} />
-      </header>
+      </Link>
+      <BurgerMenuOverlay isOpen={isMenuOpen} onClose={toggleMenu} />
+    </header>
   );
 }
 
