7ec4cc31fbd22a523bfd7c81cbe5e94fbefafe08
kenanmusali <textme0306@gmail.com>
2025-07-02 16:25:13 +0400

    footer has been updated

--- src/home/Components/Footer.jsx
+++ src/home/Components/Footer.jsx
@@ -1,6 +1,7 @@
-
-import './footer.css';
 import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FiMapPin, FiPhone, FiMail } from 'react-icons/fi';
+import './footer.css';
 
 const Footer = () => {
   return (
@@ -9,56 +10,68 @@ const Footer = () => {
         <div className="footer-content">
           
           <div className="footer-column footer-about">
-            <h3 className="footer-title">Peşəkar.az</h3>
+            <div className="footer-brand">
+              <h3 className="footer-title">Peşəkar.az</h3>
+              <div className="title-underline"></div>
+            </div>
             <p className="footer-description">
               Azərbaycanda peşəkar ustaları müştərilərlə birləşdirən platforma
             </p>
             <div className="social-icons">
-              <a href="#" aria-label="Facebook">
-                
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v2.385z"/></svg>
+              <a href="#" aria-label="Facebook" className="social-link">
+                <FaFacebook className="social-icon" />
               </a>
-              <a href="#" aria-label="X">
-                
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24h-6.617l-5.21-6.817-6.044 6.817h-3.308l7.73-8.835-7.317-10.66h6.772l4.643 6.363zm-1.102 18.25h1.7l-10.5-14.07h-1.8l10.6 14.07z"/></svg>
+              <a href="#" aria-label="Twitter" className="social-link">
+                <FaTwitter className="social-icon" />
               </a>
-              <a href="#" aria-label="Instagram">
-               
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.85s-.011 3.584-.069 4.85c-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07s-3.584-.012-4.85-.07c-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.85s.012-3.584.07-4.85c.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.85-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948s.014 3.667.072 4.947c.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072s3.667-.014 4.947-.072c4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.947s-.014-3.667-.072-4.947c-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.689-.073-4.948-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.162 6.162 6.162 6.162-2.759 6.162-6.162-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4s1.791-4 4-4 4 1.79 4 4-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.441 1.441 1.441 1.441-.645 1.441-1.441-.645-1.44-1.441-1.44z"/></svg>
+              <a href="#" aria-label="Instagram" className="social-link">
+                <FaInstagram className="social-icon" />
               </a>
-              <a href="#" aria-label="LinkedIn">
-               
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/></svg>
+              <a href="#" aria-label="LinkedIn" className="social-link">
+                <FaLinkedin className="social-icon" />
               </a>
             </div>
           </div>
 
-          
+          <div className="footer-column footer-links">
+            <h3 className="footer-title">Sürətli Keçidlər</h3>
+            <ul className="footer-nav">
+              <li><a href="#" className="footer-link">Təmir və tikinti</a></li>
+              <li><a href="#" className="footer-link">Ev və məişət xidmətləri</a></li>
+              <li><a href="#" className="footer-link">Təhsil xidmətləri</a></li>
+              <li><a href="#" className="footer-link">Gözəllik və sağlamlıq</a></li>
+              <li><a href="#" className="footer-link">Ailə və Baxıcı xidmətləri</a></li>
+              <li><a href="#" className="footer-link">İctimai və fərdi təlimlər</a></li>
+            </ul>
+          </div>
+
           <div className="footer-column footer-contact">
             <h3 className="footer-title">Əlaqə</h3>
             <ul className="contact-list">
-              <li>
-               
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>
+              <li className="contact-item">
+                <FiMapPin className="contact-icon" />
                 <span>Bakı, Azərbaycan</span>
               </li>
-              <li>
-                
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z"/></svg>
+              <li className="contact-item">
+                <FiPhone className="contact-icon" />
                 <span>+994 55 555 55 55</span>
               </li>
-              <li>
-               
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>
-                <span>info@paputi.az</span>
+              <li className="contact-item">
+                <FiMail className="contact-icon" />
+                <span>info@peshekar.az</span>
               </li>
             </ul>
           </div>
         </div>
 
-        
         <div className="footer-bottom">
-          <p>&copy;2025 Peşəkar. Bütün hüquqlar qorunur.</p>
+          <div className="footer-bottom-content">
+            <p className="copyright">&copy; {new Date().getFullYear()} Peşəkar. Bütün hüquqlar qorunur.</p>
+            <div className="legal-links">
+              <a href="#" className="legal-link">Qaydalar</a>
+              <a href="#" className="legal-link">Məxfilik Siyasəti</a>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
